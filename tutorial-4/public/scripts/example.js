6aa2ceecce06d63a74a81bb01d36d1086436a156
Rafael Salerno <salerno.rafael@gmail.com>
2016-02-19 15:08:15 -0200

    examples

--- tutorial-4/public/scripts/example.js
+++ tutorial-4/public/scripts/example.js
@@ -13,7 +13,7 @@
 
 var Comment = React.createClass({
   rawMarkup: function() {
-    var rawMarkup = marked(this.props.children.toString(), {sanitize: true});
+    var rawMarkup = marked(this.props.children.toString());
     return { __html: rawMarkup };
   },
 
